ef71a6535b76ef13c8cd846aa16477c00acfc5b9
Taha <x3727349s@iespoblenou.org>
2016-10-14 16:45:29 +0200

    Creat un ArrayList i un ArrayAdapter

--- app/src/main/java/com/example/x3727349s/visorcartasmagic/MainActivityFragment.java
+++ app/src/main/java/com/example/x3727349s/visorcartasmagic/MainActivityFragment.java
@@ -1,16 +1,23 @@
 package com.example.x3727349s.visorcartasmagic;
 
+import android.support.constraint.solver.ArrayLinkedVariables;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
@@ -21,6 +28,16 @@ public class MainActivityFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
         ListView lvCartas = (ListView) view.findViewById(R.id.lvCartas);
+
+        items = new ArrayList<>();
+        adapter = new ArrayAdapter<>(
+                getContext(),
+                R.layout.titol_cartes,
+                R.id.tvCartes,
+                items
+        );
+        lvCartas.setAdapter(adapter);
+
 
         return view;
     }
