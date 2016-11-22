a3dfc5970076b194ed81a441e98902d0cd776e85
Taha <x3727349s@iespoblenou.org>
2016-11-22 17:41:00 +0100

    Agafem les dades i les mostrem en el detall frafment

--- app/src/main/java/com/example/x3727349s/visorcartasmagic/DetailActivityFragment.java
+++ app/src/main/java/com/example/x3727349s/visorcartasmagic/DetailActivityFragment.java
@@ -1,7 +1,9 @@
 package com.example.x3727349s.visorcartasmagic;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +19,25 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        Intent i = getActivity().getIntent();
+
+        if (i != null){
+            Cartas cartas = (Cartas) i.getSerializableExtra("Cartas");
+
+            if (cartas != null){
+                updateui(cartas);
+            }
+
+
+
+        }
+
+        return view;
+    }
+
+    private void updateui(Cartas cartas) {
+        Log.d("CARTA", cartas.toString());
     }
 }
