4d05f3b4c193d5e4d40570e9d0f24dbc6e1d79d6
Taha <x3727349s@iespoblenou.org>
2016-11-29 18:04:40 +0100

    Camviem les  crides findViewById per l'objecte "binding", en els fragments i en l'activity perque les mostri

--- app/src/main/java/com/example/x3727349s/visorcartasmagic/DetailActivityFragment.java
+++ app/src/main/java/com/example/x3727349s/visorcartasmagic/DetailActivityFragment.java
@@ -1,6 +1,9 @@
 package com.example.x3727349s.visorcartasmagic;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.databinding.tool.Binding;
+import android.databinding.tool.DataBindingBuilder;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.text.Html;
@@ -12,20 +15,21 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.x3727349s.visorcartasmagic.databinding.FragmentDetailBinding;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class DetailActivityFragment extends Fragment {
 
-
-    private View view;
+    //TOT EL QUE ESTA COMENTAT ES PER FER EL CAMVI PER DATABINDING
+    /*private View view;
     private ImageView ivPosterImage;
     private TextView tvName;
     private TextView tvRarity;
     private TextView tvColor;
-    private TextView tvDescripcion;
-
+    private TextView tvDescripcion;*/
+    private FragmentDetailBinding binding;
 
     public DetailActivityFragment() {
     }
@@ -34,7 +38,12 @@ public class DetailActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         //View view = inflater.inflate(R.layout.fragment_detail, container, false);
-        view = inflater.inflate(R.layout.fragment_detail, container, false);
+        //view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        binding = DataBindingUtil.inflate( inflater, R.layout.fragment_detail, container, false);
+
+        View view = binding.getRoot();
+
 
         Intent i = getActivity().getIntent();
 
@@ -52,17 +61,23 @@ public class DetailActivityFragment extends Fragment {
     private void updateui(Cartas cartas) {
         Log.d("CARTA", cartas.toString());
 
-        ivPosterImage = (ImageView) view.findViewById(R.id.ivPosterImage);
+        /*ivPosterImage = (ImageView) view.findViewById(R.id.ivPosterImage);
         tvName = (TextView) view.findViewById(R.id.tvName);
         tvRarity = (TextView) view.findViewById(R.id.tvRarity);
         tvColor = (TextView) view.findViewById(R.id.tvColor);
-        tvDescripcion = (TextView) view.findViewById(R.id.tvDescripcion);
+        tvDescripcion = (TextView) view.findViewById(R.id.tvDescripcion);*/
 
-        tvName.setText(cartas.getName());
+        /*tvName.setText(cartas.getName());
         tvRarity.setText(cartas.getRarity());
         tvColor.setText(cartas.getColors());
         tvDescripcion.setText(cartas.getText());
-        Glide.with(getContext()).load(cartas.getImageUrl()).into(ivPosterImage);
+        Glide.with(getContext()).load(cartas.getImageUrl()).into(ivPosterImage);*/
+
+        binding.tvName.setText(cartas.getName());
+        binding.tvRarity.setText(cartas.getRarity());
+        binding.tvColor.setText(cartas.getColors());
+        binding.tvDescripcion.setText(cartas.getText());
+        Glide.with(getContext()).load(cartas.getImageUrl()).into(binding.ivPosterImage);
     }
 
 }
